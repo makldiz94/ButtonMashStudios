8bb6d295e3fd12b0e3b8e91b7546167d1d9a9f3d
makldiz94 <michael.diaz029@knights.ucf.edu>
2016-03-30 22:31:45 -0400

    Portal
    -portal prefab
    -portal.cs on player (RangeNum = 1 and 2 Possible Exits for a one-way
    portal; RangeNum = 5 and 5 Possible Exits for multi-way portals that
    take you to another random portal on the map)

---
+++ Defenders/Assets/Scripts/Portal.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Portal : MonoBehaviour {
+	public GameObject[] possibleExits;
+	public bool canTele;
+	public int teleCooldown;
+	private Transform trans;
+	private GameObject currentExit;
+	public int RangeNum;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter2D(Collider2D hit){
+		if (hit.CompareTag ("Portal")) {
+			if (canTele) {
+				int rand = Random.Range(0, RangeNum);
+				//Pick an exit
+				currentExit = possibleExits[rand];
+
+				//Don't teleport to the same portal you entered through
+				/*if (currentExit.transform.position == hit.transform.position) {
+					System.Collections.Generic.List<GameObject> list = new System.Collections.Generic.List<GameObject> (possibleExits);
+					list.Remove (currentExit);
+					possibleExits = list.ToArray ();
+				}*/
+					
+					//possibleExits.RemoveAt (1);
+			
+				//Go to the picked exit's location
+				trans = currentExit.transform;
+				transform.position = trans.position;
+				canTele = false;
+
+				//Invoke ("ResetTele", teleCooldown);
+			}
+	
+		}
+	}
+
+	void OnTriggerExit2D(Collider2D hit){
+		if (hit.CompareTag ("Portal")) {
+			canTele = true;
+			}
+
+		}
+
+
+
+
+	/*void ResetTele(){
+		canTele = true;
+	}*/
+}
